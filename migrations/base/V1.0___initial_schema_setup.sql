1eda34107c33b93b4b5cb1e815ebaa2dbb1fa4f4
Kleyton O'Hare <kleyton.ohare@gmail.com>
2022-02-09 10:42:25 +0000

    Removed 'public.' and renamed 'user' table to 'platform_user'

--- migrations/base/V1.0___initial_schema_setup.sql
+++ migrations/base/V1.0___initial_schema_setup.sql
@@ -1,103 +1,103 @@
-create table if not exists public.user_access_status
+create table if not exists user_access_status
 (
     id serial not null
         constraint user_access_status_pkey primary key,
     status varchar(100) not null
 );
 
-create table if not exists public.user
+create table if not exists platform_user
 (
     id serial not null
-        constraint user_pkey primary key,
+        constraint platform_user_pkey primary key,
     created_date date not null,
     first_name varchar(255) not null,
     last_name varchar(255) not null,
     dob date not null,
-    email varchar(255) not null
+    email varchar(100) not null
         constraint user_email_key unique,
     password varchar(50) not null,
     phone_number varchar(50),
     user_access_status_id integer not null
-        constraint user_access_status_id_fkey references public.user_access_status
+        constraint user_access_status_id_fkey references user_access_status
 );
 
-create table if not exists public.driver
+create table if not exists driver
 (
     id serial not null
         constraint driver_pkey primary key,
     license_number varchar(50),
     user_id integer not null
-        constraint user_id_fkey references public.user
+        constraint user_id_fkey references platform_user
 );
 
 
-create table if not exists public.passenger
+create table if not exists passenger
 (
     id serial not null
         constraint passenger_pkey primary key,
     user_id integer not null
-        constraint user_id_fkey references public.user
+        constraint user_id_fkey references platform_user
 );
 
-create table if not exists public.location
+create table if not exists location
 (
     id serial not null
         constraint location_pkey primary key,
     description varchar(255) not null
 );
 
-create table if not exists public.journey
+create table if not exists journey
 (
     id serial not null
         constraint journey_pkey primary key,
     created_date date not null,
     location_id_from integer not null
-        constraint location_id_from_fkey references public.location,
+        constraint location_id_from_fkey references location,
     location_id_to integer not null
-        constraint location_id_to_fkey references public.location,
+        constraint location_id_to_fkey references location,
     max_passengers integer default 0,
     driver_id integer not null
-        constraint drive_id_fkey references public.driver
+        constraint drive_id_fkey references driver
 );
 
 
-create table if not exists public.journey_occurrence
+create table if not exists journey_occurrence
 (
     id serial not null
         constraint journey_occurrence_pkey primary key,
     date_time date not null,
     journey_id integer not null
-        constraint journey_id_fkey references public.journey
+        constraint journey_id_fkey references journey
 );
 
-create table if not exists public.passenger_journey_occurrence
+create table if not exists passenger_journey_occurrence
 (
     id serial not null
         constraint passenger_journey_occurrence_pkey primary key,
     passenger_id integer not null
-        constraint passenger_id_fkey references public.passenger,
+        constraint passenger_id_fkey references passenger,
     journey_occurrence_id integer not null
-        constraint journey_occurrence_id_fkey references public.journey_occurrence
+        constraint journey_occurrence_id_fkey references journey_occurrence
 );
 
-create table if not exists public.weight
+create table if not exists weight
 (
     id serial not null
         constraint weight_pkey primary key,
     description varchar(255) not null
 );
 
-create table if not exists public.review
+create table if not exists review
 (
     id serial not null
         constraint review_pkey primary key,
     created_date date not null,
     weight integer
-        constraint weight_fkey references public.weight,
-    review varchar not null,
+        constraint weight_fkey references weight,
+    review varchar(1000) not null,
     user_id_reviewer integer not null
-        constraint user_id_reviewer_fkey references public.user,
+        constraint user_id_reviewer_fkey references platform_user,
     user_id_subject integer not null
-        constraint user_id_subject_fkey references public.user
+        constraint user_id_subject_fkey references platform_user
 );
 
