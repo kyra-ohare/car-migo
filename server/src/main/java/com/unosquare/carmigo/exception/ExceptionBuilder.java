41b520e684c670b88796c9affcd3e6d6d23c72cf
Kleyton O'Hare <kleyton.ohare@gmail.com>
2022-02-14 15:15:44 +0000

    Error handles

---
+++ server/src/main/java/com/unosquare/carmigo/exception/ExceptionBuilder.java
@@ -0,0 +1,21 @@
+package com.unosquare.carmigo.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+import java.time.LocalDateTime;
+
+public class ExceptionBuilder {
+    public static ResponseEntity<ErrorResponse> buildErrorResponseRepresentation(
+            final HttpStatus httpStatus, final String message)
+    {
+        return ResponseEntity.status(httpStatus)
+                .body(ErrorResponse.builder()
+                        .status(httpStatus.value())
+                        .path("Not specified")
+                        .error(httpStatus.getReasonPhrase())
+                        .message(message)
+                        .timestamp(LocalDateTime.now())
+                        .build());
+    }
+}
---
+++ server/src/main/java/com/unosquare/carmigo/exception/ExceptionResponseHandler.java
@@ -0,0 +1,36 @@
+package com.unosquare.carmigo.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.util.stream.Collectors;
+
+@ControllerAdvice
+public class ExceptionResponseHandler {
+
+    @ExceptionHandler
+    public ResponseEntity<ErrorResponse> handleResourceNotFoundException(final Exception exception)
+    {
+        return ExceptionBuilder.buildErrorResponseRepresentation(
+                HttpStatus.NO_CONTENT, "Resource not found."
+        );
+    }
+
+    @ExceptionHandler({MethodArgumentNotValidException.class})
+    public ResponseEntity<ErrorResponse> handleBeanValidationException(
+            final MethodArgumentNotValidException methodArgumentNotValidException)
+    {
+        return ExceptionBuilder.buildErrorResponseRepresentation(
+                HttpStatus.BAD_REQUEST,
+                "Argument not valid: " + methodArgumentNotValidException
+                        .getBindingResult()
+                        .getFieldErrors()
+                        .stream()
+                        .map(objectError -> objectError.getField() + " " + objectError.getDefaultMessage())
+                        .collect(Collectors.joining(", "))
+        );
+    }
+}
