f0f13e1c08760d2cea136a25c7bb2c83a4166ec3
Kleyton O'Hare <kleyton.ohare@gmail.com>
2022-02-14 12:16:55 +0000

    Created a PasswordValidator

---
+++ server/src/main/java/com/unosquare/carmigo/annotation/ValidPassword.java
@@ -0,0 +1,20 @@
+package com.unosquare.carmigo.annotation;
+
+import javax.validation.Constraint;
+import javax.validation.Payload;
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Documented
+@Constraint(validatedBy = PasswordValidator.class)
+@Target({ElementType.FIELD, ElementType.PARAMETER})
+@Retention(RetentionPolicy.RUNTIME)
+public @interface ValidPassword {
+    String message() default "Password must contain between 8 and 20 characters and at least 2 of the following: " +
+            "Alphanumeric characters, one special character ( @#$%^&+= ), one capital letter.";
+    Class<?>[] groups() default {};
+    Class<? extends Payload>[] payload() default {};
+}
