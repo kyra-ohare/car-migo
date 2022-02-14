f879561a256ed80d5894d3b1944b47a5b9b83ab4
Kleyton O'Hare <kleyton.ohare@gmail.com>
2022-02-14 12:20:52 +0000

    Housekeeping

--- server/src/main/java/com/unosquare/carmigo/controller/JourneyController.java
+++ server/src/main/java/com/unosquare/carmigo/controller/JourneyController.java
@@ -1,11 +1,20 @@
 package com.unosquare.carmigo.controller;
 
-import com.unosquare.carmigo.model.request.CreateJourneyViewModal;
+import com.unosquare.carmigo.model.request.CreateJourneyViewModel;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
 
-import java.util.Optional;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/v1/journeys")
@@ -27,13 +36,13 @@ public class JourneyController {
 
     @GetMapping(value = "/search", produces = MediaType.APPLICATION_JSON_VALUE)
     @ResponseStatus(HttpStatus.OK)
-    public String searchJourneys(@RequestBody final CreateJourneyViewModal createJourneyViewModal) {
+    public String searchJourneys(@RequestBody final CreateJourneyViewModel createJourneyViewModal) {
         return "Here is your search";
     }
 
     @PostMapping(produces = MediaType.APPLICATION_JSON_VALUE)
     @ResponseStatus(HttpStatus.CREATED)
-    public String createJourneys(@RequestBody final CreateJourneyViewModal createJourneyViewModal) {
+    public String createJourneys(@RequestBody final CreateJourneyViewModel createJourneyViewModal) {
         return "Journey created";
     }
 
--- server/src/main/java/com/unosquare/carmigo/controller/UserController.java
+++ server/src/main/java/com/unosquare/carmigo/controller/UserController.java
@@ -1,7 +1,6 @@
 package com.unosquare.carmigo.controller;
 
-import com.unosquare.carmigo.model.request.CreateDriverViewModal;
-import com.unosquare.carmigo.model.request.CreatePassengerViewModal;
+import com.unosquare.carmigo.model.request.CreateDriverViewModel;
 import com.unosquare.carmigo.model.request.CreateUserViewModel;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
@@ -40,7 +39,7 @@ public class UserController {
 
     @PostMapping(value = "/{id}/drivers", produces = MediaType.APPLICATION_JSON_VALUE)
     @ResponseStatus(HttpStatus.CREATED)
-    public String createDriver(@RequestBody final CreateDriverViewModal createDriverViewModal) {
+    public String createDriver(@RequestBody final CreateDriverViewModel createDriverViewModal) {
         return "Driver created";
     }
 
@@ -52,8 +51,8 @@ public class UserController {
 
     @PostMapping(value = "/{id}/passengers", produces = MediaType.APPLICATION_JSON_VALUE)
     @ResponseStatus(HttpStatus.CREATED)
-    public String createPassenger(@RequestBody final CreatePassengerViewModal createViewModal) {
-        return "Passenger created";
+    public String createPassenger(@PathVariable final int id) {
+        return "Passenger created " + id;
     }
 
     @DeleteMapping(value = "/passengers/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
@@ -62,17 +61,3 @@ public class UserController {
         return "Passenger " + id + " was deleted";
     }
 }
-
-//@GetMapping(value = "/{upc}", produces = MediaType.APPLICATION_JSON_VALUE)
-//@ApiOperation(value = "Retrieves a lookup for Product given UPC number")
-//public ResponseEntity<ProductSummaryViewModel> getProduct(@PathVariable @ValidUPC final String upc) {
-//
-
-//
-//@PostMapping(produces = MediaType.APPLICATION_JSON_VALUE)
-//@ApiOperation(value = "Create a new Favorite list by supplying a Favorite list name and it's favorite type")
-//public ResponseEntity<?> createUser(@RequestBody final CreateUserViewModel favoriteViewModel) {
-//
-//  return ResponseEntity.status(HttpStatus.CREATED);
-//}
-
