34c3cae4b9027791af0d43f9e77694bf4c23d11f
Kleyton O'Hare <kleyton.ohare@gmail.com>
2022-02-16 22:05:24 +0000

    createPassenger returns GetPassengerDTO

--- server/src/main/java/com/unosquare/carmigo/service/PassengerService.java
+++ server/src/main/java/com/unosquare/carmigo/service/PassengerService.java
@@ -1,23 +1,29 @@
 package com.unosquare.carmigo.service;
 
 import com.unosquare.carmigo.dto.CreatePassengerDTO;
+import com.unosquare.carmigo.dto.GetPassengerDTO;
 import com.unosquare.carmigo.entity.Passenger;
+import com.unosquare.carmigo.entity.PlatformUser;
 import com.unosquare.carmigo.repository.PassengerRepository;
 import lombok.RequiredArgsConstructor;
 import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
 
+import javax.persistence.EntityManager;
+
 @Service
 @RequiredArgsConstructor
 public class PassengerService
 {
-    private PassengerRepository passengerRepository;
+    private final PassengerRepository passengerRepository;
     private final ModelMapper modelMapper;
+    private final EntityManager entityManager;
 
-    public Passenger createPassenger(final CreatePassengerDTO createPassengerDTO)
+    public GetPassengerDTO createPassenger(final int id, final CreatePassengerDTO createPassengerDTO)
     {
         final Passenger passenger = modelMapper.map(createPassengerDTO, Passenger.class);
-        return passengerRepository.save(passenger);
+        passenger.setPlatformUser(entityManager.getReference(PlatformUser.class, id));
+        return modelMapper.map(passengerRepository.save(passenger), GetPassengerDTO.class);
     }
 
     public void deletePassengerById(final int id)
