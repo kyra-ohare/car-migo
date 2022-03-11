d883a0aebf1c51d35a40e7c3845639567fe0b45a
Kleyton O'Hare <kleyton.ohare@unosquare.com>
2022-03-11 10:04:13 +0000

    Impl of get all journeys and get journeys by driver's id

--- server/src/main/java/com/unosquare/carmigo/repository/JourneyRepository.java
+++ server/src/main/java/com/unosquare/carmigo/repository/JourneyRepository.java
@@ -2,9 +2,14 @@ package com.unosquare.carmigo.repository;
 
 import com.unosquare.carmigo.entity.Journey;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface JourneyRepository extends JpaRepository<Journey, Integer>
 {
+    @Query(value = "SELECT j FROM Journey j INNER JOIN j.driver d WHERE d.id = ?1")
+    List<Journey> findJourneyByDriverId(final int id);
 }
