eb83d699483005bf61258dcdee1830bdb6d9f22d
Kleyton O'Hare <kleyton.ohare@unosquare.com>
2022-05-02 21:07:41 +0100

    read Bearer from the Header

--- server/src/main/java/com/unosquare/carmigo/configuration/WebSecurityConfiguration.java
+++ server/src/main/java/com/unosquare/carmigo/configuration/WebSecurityConfiguration.java
@@ -1,5 +1,6 @@
 package com.unosquare.carmigo.configuration;
 
+import com.unosquare.carmigo.filter.JwtRequestFilter;
 import com.unosquare.carmigo.service.CarMigoUserDetailsService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
@@ -7,24 +8,27 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @EnableWebSecurity
 @RequiredArgsConstructor
 public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter
 {
-    private CarMigoUserDetailsService userDetailsService;
+    private final CarMigoUserDetailsService userDetailsService;
+    private final JwtRequestFilter jwtRequestFilter;
 
     @Override
     protected void configure(HttpSecurity httpSecurity) throws Exception
     {
         httpSecurity.csrf().disable()
                 .authorizeRequests().antMatchers("/v1/users/authenticate").permitAll().
-                anyRequest().authenticated(); //.and().
-        //                exceptionHandling().and().sessionManagement()
-        //                .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
-        //        httpSecurity.addFilterBefore(jwtRequestFilter, UsernamePasswordAuthenticationFilter.class);
+                anyRequest().authenticated().and().
+                exceptionHandling().and().sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+        httpSecurity.addFilterBefore(jwtRequestFilter, UsernamePasswordAuthenticationFilter.class);
     }
 
     @Bean
