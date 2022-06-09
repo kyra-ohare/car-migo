5ab831d1ac536cff94f8e926ffab36415e3df86d
Kleyton O'Hare <kleyton.ohare@unosquare.com>
2022-06-09 19:59:14 +0100

    refactored WebSecurityConfiguration.java

--- server/src/main/java/com/unosquare/carmigo/configuration/WebSecurityConfiguration.java
+++ server/src/main/java/com/unosquare/carmigo/configuration/WebSecurityConfiguration.java
@@ -1,25 +1,19 @@
 package com.unosquare.carmigo.configuration;
 
-import com.unosquare.carmigo.exception.AuthenticationException;
 import com.unosquare.carmigo.security.JwtRequestFilter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-import org.springframework.web.context.annotation.RequestScope;
 
 import static com.unosquare.carmigo.contant.AppConstants.ADMIN;
 import static com.unosquare.carmigo.contant.AppConstants.DEV;
-import static com.unosquare.carmigo.contant.AppConstants.NO_PERMISSIONS;
 
 @EnableWebSecurity
 @RequiredArgsConstructor
@@ -34,9 +28,12 @@ public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter
                 .authorizeRequests()
                 .antMatchers(HttpMethod.POST, "/v1/users").permitAll()
                 .antMatchers(HttpMethod.POST, "/v1/users/authenticate").permitAll()
+                .antMatchers(HttpMethod.GET, "/v1/journeys/search").permitAll()
                 .antMatchers(HttpMethod.GET, "/actuator/info").hasAuthority(DEV)
                 .antMatchers(HttpMethod.GET, "/actuator/health").hasAnyAuthority(ADMIN, DEV)
-                .antMatchers(HttpMethod.GET, "/swagger-ui/index.html").permitAll()
+                .antMatchers(HttpMethod.GET, "/swagger-ui/**").permitAll()
+                .antMatchers(HttpMethod.GET, "/v3/api-docs/swagger-config").permitAll()
+                .antMatchers(HttpMethod.GET, "/v3/api-docs**").permitAll()
                 .anyRequest().authenticated()
                 .and().sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
         httpSecurity.addFilterBefore(jwtRequestFilter, UsernamePasswordAuthenticationFilter.class);
