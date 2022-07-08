095ed17e51a66e00df7ee19beaf07e54787eddc3
Kleyton O'Hare <kleyton.ohare@unosquare.com>
2022-07-08 11:27:42 +0100

    created AuthenticationController

---
+++ server/src/main/java/com/unosquare/carmigo/controller/AuthenticationController.java
@@ -0,0 +1,41 @@
+package com.unosquare.carmigo.controller;
+
+import com.unosquare.carmigo.dto.CreateAuthenticationDTO;
+import com.unosquare.carmigo.dto.GrabAuthenticationDTO;
+import com.unosquare.carmigo.model.request.CreateAuthenticationViewModel;
+import com.unosquare.carmigo.model.response.AuthenticationViewModel;
+import com.unosquare.carmigo.service.UserService;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import javax.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.modelmapper.ModelMapper;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/v1")
+@Tag(name = "Authentication Controller")
+public class AuthenticationController {
+
+  private final ModelMapper modelMapper;
+  private final UserService userService;
+
+  @PostMapping(value = "/login", produces = MediaType.APPLICATION_JSON_VALUE)
+  @ResponseStatus(HttpStatus.CREATED)
+  public ResponseEntity<AuthenticationViewModel> createAuthenticationToken(
+      @Valid @RequestBody final CreateAuthenticationViewModel createAuthenticationViewModel) {
+    final CreateAuthenticationDTO createAuthenticationDTO = modelMapper.map(
+        createAuthenticationViewModel, CreateAuthenticationDTO.class);
+    final GrabAuthenticationDTO grabAuthenticationDTO = userService.createAuthenticationToken(createAuthenticationDTO);
+    final AuthenticationViewModel authenticationViewModel = modelMapper.map(
+        grabAuthenticationDTO, AuthenticationViewModel.class);
+    return new ResponseEntity<>(authenticationViewModel, HttpStatus.CREATED);
+  }
+}
---
+++ server/src/test/java/com/unosquare/carmigo/controller/AuthenticationControllerTest.java
@@ -0,0 +1,55 @@
+package com.unosquare.carmigo.controller;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import com.unosquare.carmigo.service.UserService;
+import com.unosquare.carmigo.util.ResourceUtility;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.modelmapper.ModelMapper;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+@ExtendWith(MockitoExtension.class)
+public class AuthenticationControllerTest {
+
+  private static final String API_ENDPOINT = "/v1/login";
+  private static final String POST_AUTHENTICATION_VALID_JSON =
+      ResourceUtility.generateStringFromResource("requestJson/PostAuthenticationValid.json");
+  private static final String POST_AUTHENTICATION_INVALID_JSON =
+      ResourceUtility.generateStringFromResource("requestJson/PostAuthenticationInvalid.json");
+
+  private MockMvc mockMvc;
+
+  @Mock private ModelMapper modelMapperMock;
+  @Mock private UserService userServiceMock;
+
+  @BeforeEach
+  public void setUp() {
+    mockMvc = MockMvcBuilders.standaloneSetup(new AuthenticationController(modelMapperMock, userServiceMock)).build();
+  }
+
+  @Test
+  public void post_Create_Authentication_Token_Returns_HttpStatus_Created() throws Exception {
+    mockMvc.perform(post(API_ENDPOINT)
+            .contentType(MediaType.APPLICATION_JSON_VALUE).content(POST_AUTHENTICATION_VALID_JSON))
+        .andExpect(status().isCreated());
+    verify(userServiceMock).createAuthenticationToken(any());
+  }
+
+  @Test
+  public void post_Create_Authentication_Token_Returns_HttpStatus_BadRequest() throws Exception {
+    mockMvc.perform(post(API_ENDPOINT)
+            .contentType(MediaType.APPLICATION_JSON_VALUE).content(POST_AUTHENTICATION_INVALID_JSON))
+        .andExpect(status().isBadRequest());
+    verify(userServiceMock, times(0)).createAuthenticationToken(any());
+  }
+}
