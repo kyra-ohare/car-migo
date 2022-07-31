d4f277abc8587a1c7ee094c30265c37194db6b33
Kleyton O'Hare <kleyton.ohare@unosquare.com>
2022-07-31 11:15:19 +0100

    test

--- server/src/test/resources/db/schema-h2.sql
+++ server/src/test/resources/db/schema-h2.sql
@@ -5,9 +5,13 @@ CREATE TABLE IF NOT EXISTS user_access_status
     status VARCHAR(100) NOT NULL
 );
 
+CREATE SEQUENCE IF NOT EXISTS platform_user_id_seq
+    INCREMENT 1
+    START 6;
+
 CREATE TABLE IF NOT EXISTS platform_user
 (
-    id INT NOT NULL
+    id INT NOT NULL DEFAULT platform_user_id_seq.nextval
         CONSTRAINT platform_user_pkey PRIMARY KEY,
     created_date DATE NOT NULL,
     first_name VARCHAR(255) NOT NULL,
@@ -21,31 +25,27 @@ CREATE TABLE IF NOT EXISTS platform_user
         CONSTRAINT user_access_status_id_fkey REFERENCES user_access_status
 );
 
-CREATE SEQUENCE IF NOT EXISTS platform_user_id_seq
+CREATE SEQUENCE IF NOT EXISTS driver_id_seq
     INCREMENT 1
     START 6;
 
 CREATE TABLE IF NOT EXISTS driver
 (
-    id INT NOT NULL
+    id INT NOT NULL DEFAULT driver_id_seq.nextval
         CONSTRAINT driver_pkey PRIMARY KEY,
     license_number VARCHAR(100),
     platform_user_id INT NOT NULL
---        CONSTRAINT platform_user_id_fkey REFERENCES platform_user ON DELETE CASCADE
-);
-
-CREATE SEQUENCE IF NOT EXISTS driver_id_seq
-    INCREMENT 1
-    START 6;
-
-CREATE TABLE IF NOT EXISTS passenger
-(
-    id INT NOT NULL
-        CONSTRAINT passenger_pkey PRIMARY KEY,
-    platform_user_id INT NOT NULL
---        CONSTRAINT platform_user_id_fkey_2 REFERENCES platform_user ON DELETE CASCADE
+        CONSTRAINT platform_user_id_fkey REFERENCES platform_user ON DELETE CASCADE
 );
 
 CREATE SEQUENCE IF NOT EXISTS passenger_id_seq
     INCREMENT 1
     START 6;
+
+CREATE TABLE IF NOT EXISTS passenger
+(
+    id INT NOT NULL DEFAULT passenger_id_seq.nextval
+        CONSTRAINT passenger_pkey PRIMARY KEY,
+    platform_user_id INT NOT NULL
+        CONSTRAINT platform_user_id_fkey_2 REFERENCES platform_user ON DELETE CASCADE
+);
