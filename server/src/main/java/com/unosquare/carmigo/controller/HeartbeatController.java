eb86d27e58347195a83193bd480490e791a2e58a
Kyra O'Hare <kyra.ohare@unosquare.com>
2023-03-29 21:14:28 +0100

    added a log

--- server/src/main/java/com/unosquare/carmigo/controller/HeartbeatController.java
+++ server/src/main/java/com/unosquare/carmigo/controller/HeartbeatController.java
@@ -5,12 +5,14 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 import java.util.HashMap;
 import java.util.Map;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/v1/heartbeat")
@@ -40,7 +42,7 @@ public class HeartbeatController {
     try {
       return userAccessStatusRepository.findById(1).isPresent();
     } catch (Exception e) {
-      System.err.println("Error occurred during DB health check. " + e.getMessage());
+      log.error("Error occurred during DB health check. " + e.getMessage());
       return false;
     }
   }
