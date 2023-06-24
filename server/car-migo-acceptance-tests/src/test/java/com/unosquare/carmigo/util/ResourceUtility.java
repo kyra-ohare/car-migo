385d93101a9ce93f96786fd7d974f95673ca2220
Kyra O'Hare <kyra.ohare@unosquare.com>
2023-06-24 20:26:03 +0100

    added remaining tests

--- server/car-migo-acceptance-tests/src/test/java/com/unosquare/carmigo/util/ResourceUtility.java
+++ server/car-migo-acceptance-tests/src/test/java/com/unosquare/carmigo/util/ResourceUtility.java
@@ -1,16 +1,10 @@
 package com.unosquare.carmigo.util;
 
-import com.fasterxml.jackson.core.type.TypeReference;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.SerializationFeature;
-import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import com.google.common.base.Charsets;
 import com.google.common.io.Resources;
 import java.io.IOException;
 
 public final class ResourceUtility {
-
-  private static final ObjectMapper objectMapper = new ObjectMapper();
 
   private ResourceUtility() {}
 
@@ -20,23 +14,5 @@ public final class ResourceUtility {
     } catch (IOException ex) {
       return "Cannot retrieve resource entity";
     }
-  }
-
-  public static byte[] convertObjectToJsonBytes(Object object) throws IOException {
-    return getCustomisedObjectMapper().writeValueAsBytes(object);
-  }
-
-  public static <T> T getObjectResponse(String jsonString) {
-    try {
-      return getCustomisedObjectMapper().readValue(jsonString, new TypeReference<>() {});
-    } catch (final Exception ex) {
-      throw new RuntimeException(ex);
-    }
-  }
-
-  private static ObjectMapper getCustomisedObjectMapper() {
-    objectMapper.registerModule(new JavaTimeModule());
-    objectMapper.configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, false);
-    return objectMapper;
   }
 }
